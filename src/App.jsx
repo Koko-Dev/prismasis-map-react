2150a8b6f88d653c1204dc62a8fa4b4c235c24a4
Koko-Dev <koko.webdev@gmail.com>
2025-04-03 12:02:27 -0400

    In src/App.jsx, change the welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // App.jsx
-import React from 'react';
 import MapComponent from './components/MapComponent';
 
 const App = () => (
     <div>
-        <h1>My Interactive Map</h1>
+        <h1>Welcome to Prismasis</h1>
         <MapComponent />
     </div>
 );
